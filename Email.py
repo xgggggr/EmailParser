9be441420b702bb45e37a0fe57d52de197c7a14c
xgggggr <a.tleuzh07@mail.ru>
2026-05-29 20:51:10 +0300

    Добавил датакласс email

---
+++ Email.py
@@ -0,0 +1,13 @@
+from dataclasses import dataclass
+from pathlib import Path
+@dataclass
+class Email:
+    source: Path #путь к эмейлу
+    format: str #формат(.txt, .jpeg, .json и т.д.)
+    subject: str #тема письма
+    who_sent: str #отправитель
+    sent_to: str #кому отправлен эмейл
+    text: str #содержание эмейла
+    is_readable: bool = True
+    def text_to_work_with(self): #метод возвращает строку для парсинга
+        return f'{self.subject} {self.who_sent} {self.sent_to} {self.text}'.lower()
